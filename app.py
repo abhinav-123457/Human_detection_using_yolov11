000c2cad3fd9f88df240f675cce0725034410015
Abhinav Shakya <136550261+abhinav-123457@users.noreply.github.com>
2025-08-17 17:43:26 +0530

    Update app.py

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ RTC_CONFIGURATION = RTCConfiguration({"iceServers": [{"urls": ["stun:stun.l.goog
 
 # Cache the model loading
 @st.cache_resource
-def load_model(model_path="best.pt"):
+def load_model(model_path="yolo11n_human_detection_final.pt"):
     try:
         return YOLO(model_path)
     except Exception as e:
